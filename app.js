f7c94c4d40243ac4afd6086c8826fbc3290f2f19
Mike MaCh <mangelmcho@gmail.com>
2020-10-21 15:25:42 -0400

    Docker y Compose configurado!

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
 
+const port = process.env.PORT || 8000;
 var app = express();
 
 // view engine setup
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -33,9 +33,18 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  // render the error page
+  //res.render('error');
+
+  // send error response
+  res.json({
+    error: err.message||err
+  });
+});
+
+app.listen(port, () => {
+  console.log('Servidor en el puerto : ' + port);
 });
 
 module.exports = app;
